e0b4c8bb8263fc6c3efce5136070fc926e7733d3
Jh2o15 <84429237+Jh2o15@users.noreply.github.com>
2025-10-27 20:24:03 +0000

    Create water_topo.tcl

---
+++ packmol/water_molecule/water_topo.tcl
@@ -0,0 +1,21 @@
+mol new waterbox.pdb waitfor all
+
+package require pbctools
+package require topotools
+
+pbc set {40.0 40.0 40.0} -all 
+
+set selO [atomselect top "name OH2 or name oh2"]
+set selH [atomselect top "name H1 or name H2 or name h1 or name h2"]
+
+$selO set type O
+$selH set type H
+$selO set charge -0.834
+$selH set charge  0.417
+$selO set mass 15.9994
+$selH set mass 1.008
+
+topo guessbonds
+topo guessangles
+
+topo writelammpsdata water.data full
