5782e3233f5d75cdd65d7db7f71f8009bc049d49
Jh2o15 <84429237+Jh2o15@users.noreply.github.com>
2025-10-27 21:09:06 +0000

    Update water_topo.tcl

--- packmol/water_molecule/water_topo.tcl
+++ packmol/water_molecule/water_topo.tcl
@@ -1,14 +1,19 @@
-
+#loads the file waterbox as a new molecule and wait untill all frames are finished.
 mol new waterbox.pdb waitfor all
 
-package require pbctools
-package require topotools
+package require pbctools #package for periodic boundary conditions
 
+#package to create/modify molecular topology data inside VMD and to export to Lammps
+package require topotools 
+
+#set periodic boundary conditions to the box, -all apply pbc to all molecules
 pbc set {40.0 40.0 40.0} -all 
 
+#it makes an identification of molecules, in case that packmol change the name of the molecules
 set selO [atomselect top "name OH2 or name oh2"]
 set selH [atomselect top "name H1 or name H2 or name h1 or name h2"]
 
+#homogenize (?) the name of the molecules and give them a charge and a mass
 $selO set type O
 $selH set type H
 $selO set charge -0.834
@@ -16,7 +21,8 @@ $selH set charge  0.417
 $selO set mass 15.9994
 $selH set mass 1.008
 
-topo guessbonds
-topo guessangles
 
-topo writelammpsdata water.data full
+topo guessbonds #ask topotools to identify atomic interaction based on interatomic distances.
+topo guessangles #after bonds are presented compute the angles. 
+
+topo writelammpsdata water.data full #write the topology of the molecule in a Lammps data file 
